36fefe8a85780b9213c3b017e3a03cc4b072b9d3
Jefferson Setiawan <jefferson.setiawan@icloud.com>
2020-04-09 10:55:11 +0700

    config example

--- Example/Render/ViewController.swift
+++ Example/Render/ViewController.swift
@@ -6,12 +6,14 @@
 //  Copyright (c) 2020 Jefferson Setiawan. All rights reserved.
 //
 
+import Render
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
